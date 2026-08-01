de2d7b526f62ff19abb8e33f2c210930901974fe
Md. Abdul Halim Khan <121864088+Nahinkh@users.noreply.github.com>
2026-08-01 23:10:33 +0600

    update css with custom css

---
+++ components/layout/navbar/Logo.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <Link href="/" className="flex items-center gap-2">
+        <div className='bg-primary-500 w-8 h-8 rounded-full flex items-center justify-center text-white font-bold'>
+
+        </div>
+
+    </Link>
+  )
+}
+
+export default Logo
